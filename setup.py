8b98c3bdd629b5f6afa570d1ecd9f01b74150d56
OptimusCoderr <chukwuebukaanulunko@gmail.com>
2024-03-23 00:49:15 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Anulunko Chukwuebuka',
+    author_email= 'chukwuebukaanulunko@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
